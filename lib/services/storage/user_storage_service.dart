b5caa7becc3a03f088bf2ce7cbcc407c0a84e613
Shrijan <ilyyhs9@gmail.com>
2020-08-15 11:02:25 +0545

    profile edit functionality added

---
+++ lib/services/storage/user_storage_service.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
+
+class UserStorage {
+  // upload user image to storage;
+  Future uploadUserImage({@required final File imgFile}) async {
+    try {
+      final _uniqueId = Uuid();
+      final _path =
+          'profile_imgs/${DateTime.now().millisecondsSinceEpoch}_${_uniqueId.v1()}';
+
+      StorageReference _ref = FirebaseStorage.instance.ref().child(_path);
+      StorageUploadTask _uploadTask = _ref.putFile(imgFile);
+      await _uploadTask.onComplete;
+      print('Upload completed!!!!');
+      final _downloadUrl = await _ref.getDownloadURL();
+      print('Success: Uploading image to firebase storage');
+      return _downloadUrl;
+    } catch (e) {
+      print(e);
+      print('Error!!!: Uploading image to firebase storage');
+      return null;
+    }
+  }
+}
