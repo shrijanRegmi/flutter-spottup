05a454a37077f8911631a4c1cdb7bbba0490b73f
Shrijan <ilyyhs9@gmail.com>
2020-08-19 14:44:47 +0545

    app name changed

--- lib/views/screens/welcome_screen.dart
+++ lib/views/screens/welcome_screen.dart
@@ -69,7 +69,7 @@ class WelcomeScreen extends StatelessWidget {
               height: 10.0,
             ),
             Text(
-              'Motel',
+              'Sppotup',
               style: TextStyle(
                 fontWeight: FontWeight.w600,
                 fontSize: 20.0,
