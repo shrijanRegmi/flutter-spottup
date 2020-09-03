a061e820170b2d843fc6efa7f149232fb3f9fa55
Shrijan <ilyyhs9@gmail.com>
2020-09-03 21:35:30 +0545

    bookings tab created

--- lib/views/screens/home/hotel_owner/home_tabs/bookings_tab.dart
+++ lib/views/screens/home/hotel_owner/home_tabs/bookings_tab.dart
@@ -1,10 +1,112 @@
 import 'package:flutter/material.dart';
+import 'package:motel/models/firebase/user_model.dart';
+import 'package:motel/viewmodels/trip_vm.dart';
+import 'package:motel/viewmodels/vm_provider.dart';
+import 'package:motel/views/screens/home/booking_tabs/contacted_booking_tab.dart';
+import 'package:motel/views/screens/home/booking_tabs/new_booking_tab.dart';
+import 'package:motel/views/screens/home/booking_tabs/other_booking_tab.dart';
+import 'package:motel/views/screens/home/trip_tabs/favourite_tab.dart';
+import 'package:motel/views/screens/home/trip_tabs/upcoming_tab.dart';
 
-class BookingsTab extends StatelessWidget {
+class BookingsTab extends StatefulWidget {
+  @override
+  _BookingsTabState createState() => _BookingsTabState();
+}
+
+class _BookingsTabState extends State<BookingsTab>
+    with SingleTickerProviderStateMixin {
+  TabController _bookingsTabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _bookingsTabController = TabController(length: 3, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return VmProvider<TripVm>(
+      vm: TripVm(),
+      builder: (context, vm, appUser) {
+        return Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                height: 20.0,
+              ),
+              _titleBuilder(),
+              _tabBarBuilder(context),
+              _tabBarViewBuilder(appUser),
+            ],
+          ),
+        );
+      },
     );
+  }
+
+  Widget _titleBuilder() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Text(
+        'Bookings',
+        style: TextStyle(
+          fontWeight: FontWeight.w600,
+          fontSize: 22.0,
+        ),
+      ),
+    );
+  }
+
+  Widget _tabBarBuilder(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Container(
+        height: 50.0,
+        decoration: BoxDecoration(
+          color: Colors.black12,
+          borderRadius: BorderRadius.circular(100.0),
+        ),
+        child: TabBar(
+          controller: _bookingsTabController,
+          indicatorColor: Colors.transparent,
+          unselectedLabelColor: Colors.black38,
+          labelColor: Color(0xff45ad90),
+          labelStyle: TextStyle(
+            fontFamily: 'Nunito',
+            fontWeight: FontWeight.bold,
+            fontSize: 13.0,
+          ),
+          tabs: <Widget>[
+            Tab(
+              text: 'New',
+            ),
+            Tab(
+              text: 'Other',
+            ),
+            Tab(
+              text: 'Contacted',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _tabBarViewBuilder(AppUser appUser) {
+    return Expanded(
+      child: TabBarView(
+        controller: _bookingsTabController,
+        children: _getTabs(appUser),
+      ),
+    );
+  }
+
+  List<Widget> _getTabs(AppUser appUser) {
+    return [
+      NewBookingTab(),
+      OtherBookingTab(),
+      ContactedBookingTab(),
+    ];
   }
 }
