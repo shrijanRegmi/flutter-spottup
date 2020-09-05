867dce8e57ab68785c1dc86911434215518a67ee
Shrijan <ilyyhs9@gmail.com>
2020-09-05 07:50:42 +0545

    booking tab completed

--- lib/views/screens/home/booking_tabs/contacted_booking_tab.dart
+++ lib/views/screens/home/booking_tabs/contacted_booking_tab.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:motel/enums/booking_type.dart';
 import 'package:motel/models/firebase/confirm_booking_model.dart';
+import 'package:motel/views/widgets/booking_tab_widgets/booking_list_item.dart';
 
 class ContactedBookingTab extends StatelessWidget {
   final List<ConfirmBooking> bookings;
   ContactedBookingTab(this.bookings);
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView.separated(
+      itemCount: bookings.length,
+      itemBuilder: (context, index) {
+        return BookingListItem(bookings[index], BookingType.contacted);
+      },
+      separatorBuilder: (context, index) {
+        return Divider();
+      },
+    );
   }
 }
--- lib/views/screens/home/booking_tabs/other_booking_tab.dart
+++ lib/views/screens/home/booking_tabs/other_booking_tab.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:motel/enums/booking_type.dart';
 import 'package:motel/models/firebase/confirm_booking_model.dart';
+import 'package:motel/views/widgets/booking_tab_widgets/booking_list_item.dart';
 
 class OtherBookingTab extends StatelessWidget {
   final List<ConfirmBooking> bookings;
   OtherBookingTab(this.bookings);
-  
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView.separated(
+      itemCount: bookings.length,
+      itemBuilder: (context, index) {
+        return BookingListItem(bookings[index], BookingType.other);
+      },
+      separatorBuilder: (context, index) {
+        return Divider();
+      },
+    );
   }
 }
