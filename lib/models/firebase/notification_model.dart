49f6c6650beaed8868b0ef41029311bdd9d00264
Shrijan <ilyyhs9@gmail.com>
2020-09-17 20:36:08 +0545

    notification tab added in hotel owner

--- lib/models/firebase/notification_model.dart
+++ lib/models/firebase/notification_model.dart
@@ -1,23 +1,46 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:motel/enums/notification_type.dart';
 
 class AppNotification {
   final String id;
   final DocumentReference hotelRef;
-  final bool isAccepted;
+  final NotificationType type;
   final bool isRead;
   final int lastUpdated;
 
-  AppNotification(
-      {this.id, this.hotelRef, this.isAccepted, this.isRead, this.lastUpdated});
+  AppNotification({
+    this.id,
+    this.hotelRef,
+    this.type,
+    this.isRead,
+    this.lastUpdated,
+  });
 
   static AppNotification fromJson(final Map<String, dynamic> data) {
     return AppNotification(
       id: data['id'],
       hotelRef: data['hotel_ref'],
-      isAccepted: data['is_accepted'] ?? false,
       isRead: data['is_read'] ?? false,
       lastUpdated:
           data['last_updated'] ?? DateTime.now().millisecondsSinceEpoch,
+      type: _gettype(data['type'] ?? 0),
     );
+  }
+}
+
+NotificationType _gettype(final type) {
+  switch (type) {
+    case 0:
+      return NotificationType.accepted;
+      break;
+    case 1:
+      return NotificationType.declined;
+      break;
+    case 2:
+      return NotificationType.bookingReceived;
+      break;
+    default:
+      return NotificationType.accepted;
+      break;
   }
 }
--- lib/views/screens/home/hotel_owner/home_screen.dart
+++ lib/views/screens/home/hotel_owner/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:motel/services/firestore/firebase_messaging_provider.dart';
+import 'package:motel/views/screens/home/home_tabs/notifications_tab.dart';
 import 'package:motel/views/screens/home/home_tabs/profile_tab.dart';
 import 'package:motel/views/screens/home/hotel_owner/home_tabs/bookings_tab.dart';
 import 'package:motel/views/screens/home/hotel_owner/home_tabs/hotels_tab.dart';
@@ -19,13 +20,14 @@ class _OwnerHomeScreenState extends State<OwnerHomeScreen>
   final _tabs = [
     HotelTab(),
     BookingsTab(),
+    NotificationsTab(),
     ProfileTab(),
   ];
 
   @override
   void initState() {
     super.initState();
-    _tabController = TabController(length: 3, vsync: this);
+    _tabController = TabController(length: 4, vsync: this);
 
     FirebaseMessagingProvider(context: context).configureMessaging();
     FirebaseMessagingProvider(uid: widget.uid).saveDevice();
@@ -87,6 +89,11 @@ class _OwnerHomeScreenState extends State<OwnerHomeScreen>
             icon: Icon(Icons.book),
             iconMargin: const EdgeInsets.all(0.0),
             text: 'Bookings',
+          ),
+          Tab(
+            icon: Icon(Icons.notifications),
+            iconMargin: const EdgeInsets.all(0.0),
+            text: 'Notifications',
           ),
           Tab(
             icon: Icon(Icons.perm_identity),
