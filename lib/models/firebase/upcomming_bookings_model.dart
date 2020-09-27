106f61bfb4db37f04de181dd1a7cbaa22da6ec7e
Shrijan <ilyyhs9@gmail.com>
2020-09-27 22:20:49 +0545

    upcomming bookings arranged based on issue date

--- lib/models/firebase/upcomming_bookings_model.dart
+++ lib/models/firebase/upcomming_bookings_model.dart
@@ -4,21 +4,24 @@ class UpcomingBooking {
   final DocumentReference hotelRef;
   final int checkIn;
   final int checkOut;
-  UpcomingBooking({this.hotelRef, this.checkIn, this.checkOut});
+  final int issueDate;
+  UpcomingBooking({this.hotelRef, this.checkIn, this.checkOut, this.issueDate});
 
   Map<String, dynamic> toJson() {
     return {
       'hotel_ref': hotelRef,
       'check_in': checkIn,
       'check_out': checkOut,
+      'issue_date': issueDate,
     };
   }
 
   static UpcomingBooking fromJson(Map<String, dynamic> data) {
     return UpcomingBooking(
       hotelRef: data['hotel_ref'] ?? '',
-      checkIn: data['check_in'] ?? 0,
-      checkOut: data['check_out'] ?? 0,
+      checkIn: data['check_in'] ?? DateTime.now().millisecondsSinceEpoch,
+      checkOut: data['check_out'] ?? DateTime.now().millisecondsSinceEpoch,
+      issueDate: data['issue_date'] ?? DateTime.now().millisecondsSinceEpoch,
     );
   }
 }
