e22c300779f307425f769dd44aae90d8d6bb904e
Shrijan <ilyyhs9@gmail.com>
2020-10-20 23:24:48 +0545

    who will pay section added in vehicle service

--- lib/views/widgets/add_new_vehicle_widgets/add_vehicle_details.dart
+++ lib/views/widgets/add_new_vehicle_widgets/add_vehicle_details.dart
@@ -2,9 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:motel/viewmodels/add_new_vehicle_vm.dart';
 import 'package:motel/views/widgets/hotels_tab_widgets/new_hotel_field.dart';
 
-class AddVehicleDetails extends StatelessWidget {
+class AddVehicleDetails extends StatefulWidget {
   final AddNewVehicleVm vm;
   AddVehicleDetails(this.vm);
+
+  @override
+  _AddVehicleDetailsState createState() => _AddVehicleDetailsState();
+}
+
+class _AddVehicleDetailsState extends State<AddVehicleDetails> {
+  var _rad1 = -1;
+  var _rad2 = -1;
+  var _rad3 = -1;
+  var _rad4 = -1;
+  var _rad5 = -1;
+  var _rad6 = -1;
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +32,11 @@ class AddVehicleDetails extends StatelessWidget {
               SizedBox(
                 height: 20.0,
               ),
-              _inputFieldContainer(vm),
+              _inputFieldContainer(widget.vm),
+              SizedBox(
+                height: 40.0,
+              ),
+              _whoWillPayBuilder(),
             ],
           ),
         ),
@@ -88,6 +104,256 @@ class AddVehicleDetails extends StatelessWidget {
           isExpanded: true,
           controller: vm.summaryController,
           requiredCapitalization: false,
+        ),
+      ],
+    );
+  }
+
+  Widget _whoWillPayBuilder() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Click the relevant box',
+          style: TextStyle(
+            fontWeight: FontWeight.w600,
+            fontSize: 14.0,
+          ),
+        ),
+        SizedBox(
+          height: 20.0,
+        ),
+        ///////////////
+        Text(
+          '1. Rent Per Day is',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        RadioListTile(
+          groupValue: _rad1,
+          value: 0,
+          onChanged: (val) {
+            setState(() {
+              _rad1 = val;
+            });
+
+            final _result = {
+              'title': 'Rent Per Day is',
+              'value': 'With Driver',
+            };
+
+            widget.vm.updateResult1(_result);
+          },
+          title: Text('With Driver'),
+        ),
+        RadioListTile(
+          groupValue: _rad1,
+          value: 1,
+          onChanged: (val) {
+            setState(() {
+              _rad1 = val;
+            });
+
+            final _result = {
+              'title': 'Rent Per Day is',
+              'value': 'Without Driver',
+            };
+
+            widget.vm.updateResult1(_result);
+          },
+          title: Text('Without Driver'),
+        ),
+        Text(
+          '2. Who Will Pay for Toll Tax',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        RadioListTile(
+          groupValue: _rad2,
+          value: 2,
+          onChanged: (val) {
+            setState(() {
+              _rad2 = val;
+            });
+
+            final _result = {
+              'title': 'Who Will Pay for Toll Tax',
+              'value': 'Company',
+            };
+
+            widget.vm.updateResult2(_result);
+          },
+          title: Text('Company'),
+        ),
+        RadioListTile(
+          groupValue: _rad2,
+          value: 3,
+          onChanged: (val) {
+            setState(() {
+              _rad2 = val;
+            });
+            final _result = {
+              'title': 'Who Will Pay for Toll Tax',
+              'value': 'Client',
+            };
+
+            widget.vm.updateResult2(_result);
+          },
+          title: Text('Client'),
+        ),
+        Text(
+          '3. Who will pay for Daily Fuel',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        RadioListTile(
+          groupValue: _rad3,
+          value: 4,
+          onChanged: (val) {
+            setState(() {
+              _rad3 = val;
+            });
+            final _result = {
+              'title': 'Who will pay for Daily Fuel',
+              'value': 'Company',
+            };
+
+            widget.vm.updateResult2(_result);
+          },
+          title: Text('Company'),
+        ),
+        RadioListTile(
+          groupValue: _rad3,
+          value: 5,
+          onChanged: (val) {
+            setState(() {
+              _rad3 = val;
+            });
+            final _result = {
+              'title': 'Who will pay for Daily Fuel',
+              'value': 'Client',
+            };
+
+            widget.vm.updateResult2(_result);
+          },
+          title: Text('Client'),
+        ),
+        Text(
+          '4. Who will pay for Driver Meal',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        RadioListTile(
+          groupValue: _rad4,
+          value: 6,
+          onChanged: (val) {
+            setState(() {
+              _rad4 = val;
+            });
+            final _result = {
+              'title': 'Who will pay for Driver Meal',
+              'value': 'Company',
+            };
+
+            widget.vm.updateResult4(_result);
+          },
+          title: Text('Company'),
+        ),
+        RadioListTile(
+          groupValue: _rad4,
+          value: 7,
+          onChanged: (val) {
+            setState(() {
+              _rad4 = val;
+            });
+            final _result = {
+              'title': 'Who will pay for Driver Meal',
+              'value': 'Client',
+            };
+
+            widget.vm.updateResult4(_result);
+          },
+          title: Text('Client'),
+        ),
+        Text(
+          '5. Who will pay for Driver Accommodation',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        RadioListTile(
+          groupValue: _rad5,
+          value: 8,
+          onChanged: (val) {
+            setState(() {
+              _rad5 = val;
+            });
+            final _result = {
+              'title': 'Who will pay for Driver Accommodation',
+              'value': 'Company',
+            };
+
+            widget.vm.updateResult5(_result);
+          },
+          title: Text('Company'),
+        ),
+        RadioListTile(
+          groupValue: _rad5,
+          value: 9,
+          onChanged: (val) {
+            setState(() {
+              _rad5 = val;
+            });
+            final _result = {
+              'title': 'Who will pay for Driver Accommodation',
+              'value': 'Client',
+            };
+
+            widget.vm.updateResult5(_result);
+          },
+          title: Text('Client'),
+        ),
+        Text(
+          '6. Who will pay for Traffic Fines',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        RadioListTile(
+          groupValue: _rad6,
+          value: 10,
+          onChanged: (val) {
+            setState(() {
+              _rad6 = val;
+            });
+            final _result = {
+              'title': 'Who will pay for Traffic Fines',
+              'value': 'Company',
+            };
+
+            widget.vm.updateResult6(_result);
+          },
+          title: Text('Company'),
+        ),
+        RadioListTile(
+          groupValue: _rad6,
+          value: 11,
+          onChanged: (val) {
+            setState(() {
+              _rad6 = val;
+            });
+            final _result = {
+              'title': 'Who will pay for Traffic Fines',
+              'value': 'Client',
+            };
+
+            widget.vm.updateResult6(_result);
+          },
+          title: Text('Client'),
         ),
       ],
     );
